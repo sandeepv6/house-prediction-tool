6e924dbc5d377c335661837de1232de64b86c0bb
ThinhLe881 <thinhleminh201@gmail.com>
2024-12-15 10:42:03 -0500

    finish backend

--- app/server.py
+++ app/server.py
@@ -1,23 +1,57 @@
 # run command:
 # uvicorn app.server:app --reload
+import joblib
+import numpy as np
+import onnxruntime as ort
 from fastapi import FastAPI
 from pydantic import BaseModel
 from geopy.geocoders import Nominatim
-import numpy as np
-import onnxruntime as ort
-
+import math
 
 app = FastAPI()
+
 ort_session = ort.InferenceSession("app/my_model.onnx")
-CLASS_LABELS = ["setosa", "versicolor", "virginica"]
+
+f_scaler = joblib.load('app/feature_scaler.joblib')
+p_scaler = joblib.load('app/price_scaler.joblib')
+
+geolocator = Nominatim(user_agent='ontariotechu')
 
 
 # Define input schema
 class InputData(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
+    num_bed: int
+    num_bath: int
+    acre_lot: float
+    house_size: float
+    address: str
+    city: str
+    state: str
+    zip_code: str
+
+
+def convert_to_cartesian(lat, lon):
+    lat_rad = math.radians(lat)
+    lon_rad = math.radians(lon)
+    x = math.cos(lat_rad) * math.cos(lon_rad)
+    y = math.cos(lat_rad) * math.sin(lon_rad)
+    z = math.sin(lat_rad)
+    return x, y, z
+
+
+def get_coordinates(location):
+    try:
+        loc = geolocator.geocode(location.split(',')[-1].strip()) # try with only the zip code
+        if loc:
+            return convert_to_cartesian(loc.latitude, loc.longitude)
+        else:
+            loc = geolocator.geocode(location)
+            if loc:
+                return convert_to_cartesian(loc.latitude, loc.longitude)
+            else:
+                return None, None, None
+    except Exception as e:
+        return None, None, None
 
 
 @app.get("/")
@@ -27,9 +61,16 @@ def read_root():
 
 @app.post("/predict")
 def predict(data: InputData):
-    input_data = np.array([[data.sepal_length, data.sepal_width, data.petal_length, data.petal_width]], dtype=np.float32)
-    outputs = ort_session.run(None, {"input": input_data})
-    logits = outputs[0]  # Get raw logits
-    predicted_class_idx = np.argmax(logits, axis=1)[0]
-    predicted_class = CLASS_LABELS[predicted_class_idx]
-    return {"prediction": predicted_class}
+    location = f'{data.address}, {data.city}, {data.state}, {data.zip_code}'
+    loc_x, loc_y, loc_z = get_coordinates(location)
+
+    if loc_x and loc_y and loc_z:
+        raw_inputs = np.array([data.num_bed, data.num_bath, data.acre_lot, data.house_size])
+        norm_inputs = f_scaler.transform([raw_inputs])
+        input_data = np.append(norm_inputs, [loc_x, loc_y, loc_z])
+        outputs = ort_session.run(None, {"input": np.expand_dims(input_data, axis=0).astype(np.float32)})
+        norm_price = outputs[0][0].item()
+        original_price = p_scaler.inverse_transform(np.array([[norm_price]]))[0][0]
+        return {"price": round(original_price), "prediction": norm_price}
+    else:
+        return {"error": "cannot find location"}
